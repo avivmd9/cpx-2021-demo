c77a73d86cbb70ed8f9b0a1c86d25904e0f53276
avivmd9 <83361585+avivmd9@users.noreply.github.com>
2021-07-13 15:19:51 +0300

    Update insecure.sh

--- docker/insecure.sh
+++ docker/insecure.sh
@@ -1,3 +1,6 @@
-API_KEY=demoprivateky
+API_KEY=xzdsa21lksamzika1kass49sjmxzm9kvaqf321d
 URL=https://coinhive.com/lib/coinhive.min.js
 URL=https://textspeier.de
+IP=134.119.219.93
+IP=122.14.131.181
+IP=45.117.140.123
